d371cc6fb746d9f5d9db7cd7470a35906ba5f153
Natalya Berdnikova <kitakava@mail.ru>
2023-06-20 15:38:41 +0500

    Add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
-  <router-view />
+  <div class="app">
+    <div class="container app__container">
+      <Navigation />
+      <router-view />
+    </div>
+  </div>
 </template>
 
 <script>
+import Navigation from './components/common/navigation/vue/Navigation.vue';
 import './index.css';
 
 export default {
   name: 'App',
+  components: {
+    Navigation,
+  },
 };
 </script>
 
---
+++ src/components/common/navigation/vue/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navigation">
+    <ul class="navigation__list">
+      <li
+        v-for="{ text, hash, attributes } in links"
+        :key="text"
+        class="navigation__list-item"
+      >
+        <router-link :to="hash" class="navigation__link" v-bind="attributes">
+          {{ text }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  computed: {
+    links() {
+      return [
+        {
+          text: 'Каталог',
+          hash: '/',
+        },
+        {
+          text: 'Корзина',
+          attributes: { 'data-link': 'bin', 'data-count': 1 },
+          hash: '/bin',
+        },
+      ];
+    },
+  },
+};
+</script>
