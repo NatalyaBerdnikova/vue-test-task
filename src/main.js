ceca4a14eeb52af58f375fa08547d85cc52a3a64
Natalya Berdnikova <kitakava@mail.ru>
2023-06-20 14:21:09 +0500

    Remove vuetify

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import { createApp } from 'vue';
-import 'vuetify/styles';
-import { createVuetify } from 'vuetify';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
 
 import App from './App.vue';
 import router from './router';
 
-const vuetify = createVuetify({
-  components,
-  directives,
-});
-
-createApp(App).use(vuetify).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
